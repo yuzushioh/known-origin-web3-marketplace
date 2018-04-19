287c6eb388f223dd9510740b16f7b5d0d9544459
Andy Gray <andy@whatsthescore.co.uk>
2018-04-19 10:09:32 +0100

    scss

--- src/components/GalleryEdition.vue
+++ src/components/GalleryEdition.vue
@@ -1,8 +1,11 @@
 <template>
 
   <div class="card" v-if="edition">
+
     <img class="card-img-top" :src="edition.lowResImg"/>
+
     <!--<asset-figure :edition="edition"></asset-figure>-->
+
     <div class="card-body">
 
       <edition-name-by-artist :edition="edition" :purchase="purchase"></edition-name-by-artist>
@@ -11,17 +14,21 @@
 
       <p class="card-text"><small class="text-muted">{{ availableAssetsForEdition(edition.edition).length }} available</small></p>
     </div>
+
     <ul class="list-group list-group-flush">
       <li class="list-group-item"><price-in-eth :value="edition.priceInEther"></price-in-eth></li>
     </ul>
+
     <div class="card-footer text-center" v-if="!purchase">
       <router-link :to="{ name: 'confirmPurchase', params: { artistCode: edition.edition.substring(0, 3), edition: edition.edition }}">
         View details
       </router-link>
     </div>
+
     <div class="card-footer" v-if="purchase">
       <confirm-purchase-button :edition="edition"></confirm-purchase-button>
     </div>
+
   </div>
 </template>
 
@@ -55,5 +62,5 @@
   };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 </style>
