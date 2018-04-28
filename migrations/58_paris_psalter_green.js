e29782cde1acdbd9fb825fbe59f8dddd9fe3c559
Andy Gray <andy@whatsthescore.co.uk>
2018-04-28 15:42:12 +0100

    better formatting

--- migrations/58_paris_psalter_green.js
+++ migrations/58_paris_psalter_green.js
@@ -24,22 +24,17 @@ const artistAccount = "0x8d01Bdf55Fa7f1CCfef7b670a11B8c14faf827Bf";
 
 module.exports = async function (deployer, network, accounts) {
 
-  const {_curatorAccount, _developerAccount, _artistAccount} = loadContractCredentials(network, accounts, artistAccount);
-
-  console.log(`Running within network = ${network}`);
-
   let instance = await KnownOriginDigitalAsset.deployed();
-  const _openingTime = await blocktimestampPlusOne(web3);
-
   console.log(`Deployed contract to address = [${instance.address}] to network [${network}]`);
 
+  const {_curatorAccount, _developerAccount, _artistAccount} = loadContractCredentials(network, accounts, artistAccount);
+  const _openingTime = await blocktimestampPlusOne(web3);
+
   if (network === 'ganache' || network === 'live' || network === 'ropsten' || network === 'rinkeby') {
-    console.log(`Loading in seed data...`);
     return loadSeedData(instance, _artistAccount, _openingTime, galleryData, _developerAccount);
   } else {
     console.log(`SKIPPING loading seed data as running on ${network}`);
   }
 
   return instance;
-
 };
